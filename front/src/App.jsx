ab602042885d4a16575713b8c87dc4a2464c94a5
Deividas Jasas <167405264+DeividasJas@users.noreply.github.com>
2024-07-10 10:47:11 +0300

    Added tailwind and daisy

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -5,7 +5,10 @@ function App() {
   const { hello } = useContext(AppContext);
   console.log(hello);
 
-  return <></>;
+  return <>
+  <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1></>;
 }
 
 export default App;
