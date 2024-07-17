418ca2faa6086548994d41de3b9d580e6e8331cc
Deividas Jasas <167405264+DeividasJas@users.noreply.github.com>
2024-07-17 16:03:56 +0300

    Bug fixes

--- back/controllers/recipesController.mjs
+++ back/controllers/recipesController.mjs
@@ -4,39 +4,34 @@ import {
   pg_patchRecipe,
   pg_getAllRecipes,
   pg_getRecipesByUserId,
-  pg_getRecipeByIdWithSocials
+  pg_getRecipeByIdWithSocials,
 } from '../models/recipesModel.mjs';
 
 export const postRecipe = async (req, res) => {
-  console.log('IMA FROM POST');
   try {
     let { title, ingredients, steps, category, cuisine, image } = req.body;
-    console.log(req.body);
-
     const { id } = req.user;
 
     ingredients.forEach((obj) => {
       if (obj.ingredient) {
-        obj.ingredient = obj.ingredient.toLowerCase();
+        obj.ingredient = obj.ingredient.toLowerCase().trim();
+        obj.amount = obj.ingredient.toLowerCase().trim();
       }
     });
     steps.forEach((step, index) => {
-      steps[index] = step.toLowerCase();
+      steps[index] = step.toLowerCase().trim();
     });
-    cuisine = cuisine.toLowerCase();
-
-    //  console.log(title, ingredients, steps, category, cuisine, image, id, 'I AM FROM RECIPES CONTROLLER BEFORE ' );
+    cuisine = cuisine.toLowerCase().trim();
 
     const newRecipe = await pg_postRecipe(
-      title,
+      title.trim(),
       ingredients,
-      steps, 
-      category.toLowerCase(),
+      steps,
+      category.toLowerCase().trim(),
       cuisine,
-      image,
+      image.trim(),
       id
     );
-    console.log(newRecipe);
     res.status(201).json(newRecipe);
   } catch (error) {
     console.error(error);
@@ -60,27 +55,38 @@ export const patchRecipe = async (req, res) => {
     const recipeId = req.params.id;
     // console.log(recipeId);
     let { title, ingredients, steps, category, cuisine, image } = req.body;
-    console.log(title, ingredients, steps, category, cuisine, image, 'im image' );
-    steps.forEach((step, index) => {
-      steps[index] = step.toLowerCase();
-    });
-    ingredients.forEach((obj) => {
-      if (obj.ingredient) {
-        obj.ingredient = obj.ingredient.toLowerCase();
-      }
-    });
-    cuisine = cuisine.toLowerCase();
-    
-    // console.log(recipeId, title, ingredients, steps, category, cuisine, image);
-    const patchedRecipe = await pg_patchRecipe(
-      recipeId,
+    console.log(
       title,
       ingredients,
       steps,
       category,
       cuisine,
-      image
+      image,
+      'im image'
     );
+    steps.forEach((step, index) => {
+      steps[index] = step.toLowerCase().trim();
+    });
+    ingredients.forEach((obj) => {
+      if (obj.ingredient) {
+        obj.ingredient = obj.ingredient.toLowerCase().trim();
+        obj.amount = obj.ingredient.toLowerCase().trim();
+      }
+    });
+    // cuisine = cuisine.toLowerCase().trim();
+
+    // console.log(recipeId, title, ingredients, steps, category, cuisine, image);
+    const patchedRecipe = await pg_patchRecipe(
+      recipeId,
+      title.trim(),
+      ingredients,
+      steps,
+      category.trim(),
+      cuisine.toLowerCase().trim(),
+      image.trim()
+    );
+    console.log(patchedRecipe);
+
     res.status(200).json(patchedRecipe);
   } catch (error) {
     console.error(error);
@@ -91,12 +97,11 @@ export const patchRecipe = async (req, res) => {
 export const getRecipes = async (req, res) => {
   try {
     const user = req.user;
-    console.log(user.role, ' - is user role');
 
     if (user.role === 'user') {
       console.log(user.id, ' - is user id');
       const recipes = await pg_getRecipesByUserId(user.id);
-      console.log(recipes);
+
       res.status(200).json(recipes);
     } else {
       const recipes = await pg_getAllRecipes();
@@ -111,12 +116,11 @@ export const getRecipes = async (req, res) => {
 
 export const getRecipeByIdWithSocials = async (req, res) => {
   try {
-    const { id } = req.params
-    const recipe = await pg_getRecipeByIdWithSocials(id)
-    res.status(200).json(recipe)
-
+    const { id } = req.params;
+    const recipe = await pg_getRecipeByIdWithSocials(id);
+    res.status(200).json(recipe);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: error });
   }
-}
+};
--- front/src/services/post.mjs
+++ front/src/services/post.mjs
@@ -18,14 +18,12 @@ export const postRecipe = async (formInfo) => {
 export const postNewCategory = async (categoryName) => {
   const token = window.localStorage.getItem('token');
   const post_cat_url = import.meta.env.VITE_POST_CAT;
-  console.log(post_cat_url);
   try {
     const config = {
       headers: { Authorization: `Bearer ${token}` },
     };
 
     const response = await axios.post(post_cat_url, categoryName, config);
-    console.log(response, 'im from post');
     return response;
   } catch (error) {
     console.error(error);
