cc8d6f5330c22219117e143780df4a5e70c89e96
Deividas Jasas <167405264+DeividasJas@users.noreply.github.com>
2024-07-17 08:54:27 +0300

    Back end ratings

--- back/controllers/socialController.mjs
+++ back/controllers/socialController.mjs
@@ -1,11 +1,30 @@
-import {pg_addComment} from '../models/socialModel.mjs'
+import {
+  pg_addReview,
+  pg_getReviewByRecipeId,
+} from '../models/socialModel.mjs';
 
-export const addComment = async (req, res) => {
+export const addReview = async (req, res) => {
   try {
-    req.body
+    const { id: userId } = req.user;
+    console.log(req.user);
+    const { comment, recipeId, rating } = req.body;
+    const review = await pg_addReview(recipeId, userId, comment, rating);
+    console.log(review);
+    res.status(201).json(review);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: error });
   }
-}
+};
 
+export const getReviewByRecipeId = async (req, res) => {
+  try {
+    const { id: recipeId } = req.params;
+    const recipeReviews = await pg_getReviewByRecipeId(recipeId);
+    console.log(recipeReviews);
+    res.status(200).json(recipeReviews);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error });
+  }
+};
--- back/routes/socialRoute.mjs
+++ back/routes/socialRoute.mjs
@@ -1,9 +1,12 @@
 import express from 'express'
+import { addReview, getReviewByRecipeId } from '../controllers/socialController.mjs'
+import {isUser} from '../middlewares/authorizationMiddleware.mjs'
+
 
 const router = express.Router()
 
-router.route('/comment')
-router.route('/like')
+router.route('/').post(isUser, addReview)
 router.route('/follow')
+router.route('/:id').get(isUser, getReviewByRecipeId)
 
 export default router
