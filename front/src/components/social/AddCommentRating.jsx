d910fa95e89e00160892894788c1fd3fe766063b
Deividas Jasas <167405264+DeividasJas@users.noreply.github.com>
2024-07-18 14:59:35 +0300

    Created comment box inside big recipe card

--- front/src/components/social/AddCommentRating.jsx
+++ front/src/components/social/AddCommentRating.jsx
@@ -1,4 +1,3 @@
-
 import { useState, useContext } from 'react';
 import Rating from '@mui/material/Rating';
 import Box from '@mui/material/Box';
@@ -7,52 +6,60 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { Controller, useForm } from 'react-hook-form';
- 
- 
- 
-const labels = {
-    0.5: 'Useless',
-    1: 'Useless+',
-    1.5: 'Poor',
-    2: 'Poor+',
-    2.5: 'Ok',
-    3: 'Ok+',
-    3.5: 'Good',
-    4: 'Good+',
-    4.5: 'Excellent',
-    5: 'Excellent+',
-  };
- 
-  function AddCommentRating({recipeData}) {
-    console.log(recipeData);
-    const formSubmitHandler = async (commentData) => {
-      commentData.recipeId = recipeData.recipeId
-      console.log(commentData);
+import { postReview } from '../../services/post.mjs';
+import toast, { Toaster } from 'react-hot-toast';
 
-      try {
-        
-      } catch (error) {
-        console.log(error);
+const labels = {
+  0.5: 'Useless',
+  1: 'Useless+',
+  1.5: 'Poor',
+  2: 'Poor+',
+  2.5: 'Ok',
+  3: 'Ok+',
+  3.5: 'Good',
+  4: 'Good+',
+  4.5: 'Excellent',
+  5: 'Excellent+',
+};
+
+function AddCommentRating({ recipeData }) {
+  // console.log(recipeData);
+
+  const formSubmitHandler = async (commentData) => {
+    try {
+      const newData = {
+        ...commentData,
+        recipeId: recipeData.recipeid,
+      };
+      const isPosted = await postReview(newData);
+      console.log(isPosted);
+      if(isPosted.status = 201){
+        toast.success('Comment is posted')
+      } else {
+        toast.error('Error is not posted')
       }
-    };
- 
-    const [hover, setHover] = useState(-1);
- 
-    const {
-        control,
-        handleSubmit,
-        reset,
-        formState: { errors },
-      } = useForm({
-        defaultValues: {
-          comment: "",
-          rating: 2.5,
-        },
-      });
- 
-    return (
-<Box
-      component="form"
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const [hover, setHover] = useState(-1);
+
+  const {
+    control,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      comment: '',
+      rating: 2.5,
+    },
+  });
+
+  return (
+    <Box
+      component='form'
       onSubmit={handleSubmit(formSubmitHandler)}
       style={{
         display: 'flex',
@@ -68,15 +75,15 @@ const labels = {
       }}
     >
       <Controller
-        name="comment"
+        name='comment'
         control={control}
         render={({ field }) => (
           <TextField
             {...field}
-            label="Write your comment"
+            label='Write your comment'
             multiline
             rows={3}
-            variant="outlined"
+            variant='outlined'
             fullWidth
             error={!!errors.comment}
             helperText={errors.comment ? 'Comment is required' : ''}
@@ -84,12 +91,20 @@ const labels = {
         )}
         rules={{ required: true }}
       />
+
       <Controller
-        name="rating"
+        name='rating'
         control={control}
         render={({ field }) => (
-          <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '16px' }}>
-            <Typography variant="h6" gutterBottom>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              gap: '16px',
+            }}
+          >
+            <Typography variant='h6' gutterBottom>
               Rate your excursion below
             </Typography>
             <Box
@@ -109,23 +124,26 @@ const labels = {
                 onChangeActive={(event, newHover) => {
                   setHover(newHover);
                 }}
-                emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
+                emptyIcon={
+                  <StarIcon style={{ opacity: 0.55 }} fontSize='inherit' />
+                }
               />
               {field.value !== null && (
-                <Box sx={{ ml: 2 }}>{labels[hover !== -1 ? hover : field.value]}</Box>
+                <Box sx={{ ml: 2 }}>
+                  {labels[hover !== -1 ? hover : field.value]}
+                </Box>
               )}
             </Box>
           </Box>
         )}
         rules={{ required: true }}
       />
-      <Button type="submit" variant="contained" color="primary">
+      <Button type='submit' variant='contained' color='primary'>
         Add comment
       </Button>
+      <Toaster/>
     </Box>
- 
-     );
-  }
- 
-  export default AddCommentRating;
- 
+  );
+}
+
+export default AddCommentRating;
--- front/src/components/social/CommentRaitingCards.jsx
+++ front/src/components/social/CommentRaitingCards.jsx
@@ -1,12 +1,14 @@
 import Rating from '@mui/material/Rating';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import { useParams } from "react-router-dom";
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import Button from '@mui/joy/Button';
 
+import { useParams } from 'react-router-dom';
 
-const AddCommentRating = ({recipeData}) => {
+const AddCommentRating = ({ recipeData }) => {
   const { id } = useParams();
-
+  // console.log(recipeData);
 
   const labels = {
     0.5: 'Useless',
@@ -21,47 +23,56 @@ const AddCommentRating = ({recipeData}) => {
     5: 'Excellent+',
   };
 
-//   const filteredUserTours = users.map(user => ({
-//     ...user,
-//     tours: user.tours.filter(tour => tour.tourId._id === id)
-//   }));
+  // need to finish like functionllity
+  const handleLike = () => {};
 
   return (
-    <Box>
-      {/* {filteredUserTours.map((user) => (
-        user.tours.map((tour, index) => ( */}
-          <Box 
-            // key={index} 
-            style={{
-              width: '500px',
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'flex-start',
-              justifyContent: 'center',
-              padding: '20px',
-              margin: 'auto',
-            //   backgroundColor: '#f5f5f5',
-              borderRadius: '8px',
-            }}
-          >
-            {/* map this bad boi */}
-            <Typography variant="body1" gutterBottom>
-              Comment: {["hello ", "du"]}
-            </Typography>
+    <Box
+      // key={index}
+      style={{
+        maxWidth: '700px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        justifyContent: 'center',
+        padding: '20px',
+        margin: 'auto',
+        //   backgroundColor: '#f5f5f5',
+        borderRadius: '8px',
+      }}
+    >
+      <div className='w-full border shadow-md flex rounded-lg'>
+        <div className='w-full flex flex-col justify-start py-2 px-5 divide-y-2 space-y-2'>
+          {recipeData.social.comments.map((comment, index) => {
+            return (
+              <div className='flex' key={index}>
+                <div key={index} className='w-full flex flex-col ml-3'>
+                  <div className='w-full flex flex-col  justify-between mt-2'>
+                    <p className='capitalize'>
+                      {comment.commenter_lastname} {comment.commenter_name},
+                    </p>
+                  </div>
+                  <p className=' mt-2'>Commented: {comment.comment_text}</p>
+                </div>
 
-
-            <Typography variant="h6" gutterBottom>
-            Average rating of
-            </Typography>
-            <Rating
-              value={recipeData.average_rating}
-              precision={0.5}
-              readOnly
-            />
-            {/* <Typography variant="body1">{labels[recipe.rating]}</Typography> */}
-          </Box>
-        {/* ))
-      ))} */}
+                <div className='flex flex-col mr-3'>
+                  <p className=' text-sm text-right'>{comment.comment_date}</p>
+                  <div className='text-right mt-2'>
+                    <Button
+                      sx={{ width: '' }}
+                      color='danger'
+                      variant='outlined'
+                      onClick={handleLike}
+                    >
+                      <FavoriteBorderIcon color='' />
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </Box>
   );
 };
