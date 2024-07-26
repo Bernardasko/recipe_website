fe16d450e77eca09a06a5f2e7979919223eef549
imileskiene <ibitautaite@gmail.com>
2024-07-26 10:41:13 +0300

    Change button

--- front/src/components/Profile.jsx
+++ front/src/components/Profile.jsx
@@ -40,7 +40,7 @@ function Profile() {
           )}
 
           <Link to={'/profile/followers'}>
-            <button id='buttonprof' className='px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 w-full'>
+            <button id='buttonproffollow' className='px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 w-full'>
               followers
             </button>
           </Link>
